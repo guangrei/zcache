9a50a81decae4965963f822160b49b160d516a72
guangrei <20879950+guangrei@users.noreply.github.com>
2024-10-13 22:38:27 +0700

    fix test async dictstorage

--- Tests/test_async_dicstorage.py
+++ Tests/test_async_dicstorage.py
@@ -6,9 +6,11 @@ from zcache.Storage.AsyncDictStorage import AsyncDictStorage
 
 class DBTest(IsolatedAsyncioTestCase):
 
-    async def test_database_or_cache(self):
-        c = await AsyncDatabase("/tmp/async_dicstorage_test.json", storage=AsyncDictStorage)
+    async def test_database_or_cache(self) -> None:
+        storage = AsyncDictStorage("test")
+        await storage.init()
+        c = AsyncDatabase(storage=storage)
+        await c.init()
         await c.reset()
-
         expected = await c.set("foo", "bar")
         self.assertEqual(expected, True)
